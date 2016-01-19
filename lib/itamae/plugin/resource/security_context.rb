c1f04f80349ad22701f2c5737604026f3f178a19
KITAITI Makoto <KitaitiMakoto@gmail.com>
2016-01-19 17:45:45 +0900

    Implement restore action of security_context resource

--- lib/itamae/plugin/resource/security_context.rb
+++ lib/itamae/plugin/resource/security_context.rb
@@ -1,1 +1,19 @@
 require 'itamae/plugin/resource/security_context/version'
+
+module Itamae
+  module Plugin
+    module Resource
+      class SecurityContext < ::Itamae::Resource::Base
+        define_attribute :action, default: :nothing
+        define_attribute :path, type: [String], default_name: true
+        define_attribute :recursive, type: [TrueClass, FalseClass], default: false
+
+        def action_restore(options)
+          cmd = ['restorecon', attributes.path]
+          cmd << '-R' if attributes.recursive
+          run_command cmd
+        end
+      end
+    end
+  end
+end
